0f5c688e71fb04cab9ec2634a92304a366320702
Eric Tierre <erictierre29@gmail.com>
2019-08-08 10:38:05 -0400

    Update teste.py

--- teste.py
+++ teste.py
@@ -1,29 +1,14 @@
-# ------------------------------------------------------------
-# calclex.py
-#
-# tokenizer for a simple expression evaluator for
-# numbers and +,-,*,/
-# ------------------------------------------------------------
 import ply.lex as lex
-'''
+
 reserved = {
 	'void'	:	'VOID',
 	'int'	:	'INT',
-}'''
+}
 
 # List of token names.   This is always required
-tokens = (
-	'NUMBER',
-	'PLUS',
-	'MINUS',
-	'TIMES',
-	'DIVIDE',
-	'LPAREN',
-	'RPAREN',
-	'void',
-)
-tokens.append
-print(type(tokens))
+tokens = ['NUMBER','PLUS','MINUS','TIMES','DIVIDE','LPAREN','RPAREN',
+	'ID',
+	]+ list(reserved.values())
  
 # Regular expression rules for simple tokens
 t_PLUS    = r'\+'
@@ -32,27 +17,26 @@ t_TIMES   = r'\*'
 t_DIVIDE  = r'/'
 t_LPAREN  = r'\('
 t_RPAREN  = r'\)'
-t_void  = r'void'
 
-#literals = "void"
+# A string containing ignored characters (spaces and tabs)
+t_ignore  = ' \t'
+
 # A regular expression rule with some action code
 def t_NUMBER(t):
 	r'\d+'
 	t.value = int(t.value)
 	return t
-'''	
-def t_void(t):
-	r'\dvoid'
-	t.type = "void" 
+	
+def t_ID(t):
+	r'[a-zA-Z_][a-zA-Z_0-9]*'
+	if t.value in reserved:# Check for reserved words
+		t.type = reserved[ t.value ]
 	return t
-'''
+
 # Define a rule so we can track line numbers
 def t_newline(t):
 	r'\n+'
 	t.lexer.lineno += len(t.value)
-
-# A string containing ignored characters (spaces and tabs)
-t_ignore  = ' \t'
 
 # Error handling rule
 def t_error(t):
@@ -61,7 +45,6 @@ def t_error(t):
 
 # Build the lexer
 lexer = lex.lex()
-print(type(lexer))
 
 def main():
 	teste = open('codigo.txt', 'r')
